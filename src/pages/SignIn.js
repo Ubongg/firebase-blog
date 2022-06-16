8dbc6767d555cddb0949104c6baa48fcdf229e9e
Ubong Effiong <UbongEA@Ubongs-MacBook-Pro.local>
2022-06-16 12:41:58 +0100

    changed styles

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -78,10 +78,6 @@ const SignIn = () => {
             />
           </div>
 
-          <Link to="/forgot-password" className="forgotPasswordLink">
-            Forgot Password
-          </Link>
-
           <div className="signInBar">
             <p className="signInText">Sign In</p>
             <button className="signInButton">
